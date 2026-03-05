15fe61ebbe99e8860c64985960c1bf24c17d37f3
heesik <cka8701@gmail.com>
2026-03-05 13:07:17 +0900

    first commit

---
+++ src/main/java/learntime/backend/global/error/CustomExceptionHandler.java
@@ -0,0 +1,49 @@
+package learntime.backend.global.error;
+
+import learntime.backend.global.dto.ErrorResponseDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
+
+// 전역 예외 관리
+@Slf4j
+@RestControllerAdvice
+public class CustomExceptionHandler {
+
+    // 설정한 예외 외의 모든 예외 처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponseDTO> handle(Exception error) {
+        log.error("서버 내부 오류 발생: ", error);
+
+        ErrorResponseDTO body = new ErrorResponseDTO(
+                "500",
+                "서버 오류입니다. 잠시 후 다시 접속해주세요.",
+                error.getMessage(),
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(body);
+    }
+
+    // ResponseStatusException 예외 처리
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ErrorResponseDTO> handleStatus(ResponseStatusException e) {
+        ErrorResponseDTO body = new ErrorResponseDTO(
+                e.getStatusCode().toString(),
+                e.getReason() != null ? e.getReason() : "요청 처리 중 오류가 발생했습니다.",
+                "",
+                LocalDateTime.now()
+        );
+
+        return ResponseEntity
+                .status(e.getStatusCode())
+                .body(body);
+    }
+}
---
+++ src/test/java/learntime/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package learntime.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
