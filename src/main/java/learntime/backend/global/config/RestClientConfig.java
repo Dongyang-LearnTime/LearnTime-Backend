ac465a18050e764541416fd32d49c78a48dcef4b
Jinpiter <jjw2963@googlemail.com>
2026-03-17 23:26:15 +0900

    [#3]; feat: Gemini API를 통한 타임아웃 시간을 30초에서 60초로 완화

--- src/main/java/learntime/backend/global/config/RestClientConfig.java
+++ src/main/java/learntime/backend/global/config/RestClientConfig.java
@@ -12,7 +12,7 @@ public class RestClientConfig {
     public RestClient restClient() {
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
         factory.setConnectTimeout(5000); // 5초 [cite: 16]
-        factory.setReadTimeout(30000);    // 30초 [cite: 16]
+        factory.setReadTimeout(60000);    // 60초 [cite: 16]
 
         return RestClient.builder()
                 .requestFactory(factory)
