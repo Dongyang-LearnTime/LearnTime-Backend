14e6e65c27f74d54bac8f24e3f7c18a716a6e457
Jinpiter <jjw2963@googlemail.com>
2026-03-17 20:58:54 +0900

    [#3]; feat: Gemini API 통신과 필요한 정보 파일 GeminiClient 추가

---
+++ src/main/java/learntime/backend/global/infra/gemini/GeminiClient.java
@@ -0,0 +1,37 @@
+package learntime.backend.global.infra.gemini;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClient;
+import java.util.Map;
+
+@Component
+public class GeminiClient {
+
+    @Value("${gemini.api.key}")
+    private String apiKey;
+
+    // gemini-2.5-flash
+    private final String GEMINI_URL = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
+
+    private final RestClient restClient;
+
+    public GeminiClient(RestClient restClient) {
+        this.restClient = restClient;
+    }
+
+    public String sendRequest(Map<String, Object> requestBody) {
+        return restClient.post()
+                .uri(GEMINI_URL + "?key=" + apiKey)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(requestBody)
+                .retrieve()
+                // 에러 발생 시 상세 메시지 출력
+                .onStatus(status -> status.is4xxClientError(), (request, response) -> {
+                    throw new RuntimeException("Gemini API Client Error: " + response.getStatusText() +
+                            " | Body: " + new String(response.getBody().readAllBytes()));
+                })
+                .body(String.class);
+    }
+}
