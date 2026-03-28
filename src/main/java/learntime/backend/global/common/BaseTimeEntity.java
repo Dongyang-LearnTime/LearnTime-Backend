139456c15a224b14aff137f5414976d4f6200155
Jinpiter <jjw2963@googlemail.com>
2026-03-28 10:50:03 +0900

    [#8]; feat: 7일간의 데이터 수집에 필요한 시간 관리 클래스 BaseTimeEntity 추가

---
+++ src/main/java/learntime/backend/global/common/BaseTimeEntity.java
@@ -0,0 +1,20 @@
+package learntime.backend.global.common;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    @Column(updatable = false)
+    // 기록 날짜 및 시간을 자동으로 관리하는 클래스 (DB의 모든 생성일 정보를 여기서 할당)
+    private LocalDateTime createAt;
+}
