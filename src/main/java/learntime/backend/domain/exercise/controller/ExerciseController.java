fdad347ebc5cbd9313e241d597acefe18639e392
Jinpiter <jjw2963@googlemail.com>
2026-05-13 11:50:15 +0900

    [#34]; feat: Swagger 상의 API별 텍스트 설명 추가

--- src/main/java/learntime/backend/domain/exercise/controller/ExerciseController.java
+++ src/main/java/learntime/backend/domain/exercise/controller/ExerciseController.java
@@ -1,5 +1,7 @@
 package learntime.backend.domain.exercise.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import learntime.backend.domain.exercise.dto.request.ExerciseRequestDTO;
 import learntime.backend.domain.exercise.dto.response.ExerciseResponseDTO;
@@ -18,17 +20,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/exercise")
 @RequiredArgsConstructor
+@Tag(name = "운동 기능 API", description = "추천 유튜브 영상을 가져오고, 수행한 운동 저장을 담당 (JWT 필요)")
 public class ExerciseController {
 
     private final ExerciseService exerciseService;
 
     @GetMapping("/videos")
+    @Operation(summary = "추천 운동 영상 조회", description = "사용자가 선택한 신체부위에 해당하는 Youtube id를 조회합니다.")
     public ResponseEntity<List<YoutubeVideoResponseDTO>> getVideos(@RequestParam List<String> bodyParts) {
         List<YoutubeVideoResponseDTO> videos = exerciseService.getRecommendedVideos(bodyParts);
         return ResponseEntity.ok(videos);
     }
 
     @PostMapping("/save")
+    @Operation(summary = "운동 기록 저장", description = "운동 부위, 시간(분), 내용을 저장합니다.")
     public ResponseEntity<ExerciseResponseDTO> saveExercise(
             @AuthenticationPrincipal CustomUserDetails user,
             @Valid @RequestBody ExerciseRequestDTO request
